4e5f554840f570b32dd36c97d08d39312e6e9a65
ojbkk <Qinyuchao96@163.com>
2018-01-07 23:30:21 +0800

    新增shopcart，cartcontrol组件

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -29,19 +29,23 @@
                                   <span class="price now">￥{{ food.price }}</span>
                                   <span v-show="food.oldPrice" class="old">{{ food.oldPrice }}</span>
                             </div>
+                            <div class="cartcontrol-wrapper">
+                                <cartcontrol :food="food"></cartcontrol>
+                            </div>
                         </div>
                     </li>
                 </ul>
             </li>
         </ul>
     </div>
-    <shopcart :deliveryprice="seller.deliveryPrice" :minprice="seller.minPrice"></shopcart>
+    <shopcart :selectFoods="selectFoods" :deliveryprice="seller.deliveryPrice" :minprice="seller.minPrice"></shopcart>
 </div>
 </template>
 
 <script>
 import BScroll from 'better-scroll';
-import shopcart from '../shopcart/shopcart'
+import shopcart from '../shopcart/shopcart';
+import cartcontrol from '../cartcontrol/cartcontrol';
  const ERR_OK = 0;
 
  export default {
@@ -70,6 +74,17 @@ import shopcart from '../shopcart/shopcart'
         }
 
         return 0;
+    },
+    selectFoods(){
+        let foods = [];
+        this.goods.forEach((good)=>{
+            good.foods.forEach((food)=>{
+                if(food.count){
+                    foods.push(food);
+                }
+            })
+        })
+        return foods
     }  
   },
   created () {
@@ -93,7 +108,9 @@ import shopcart from '../shopcart/shopcart'
             let foodList = this.$refs.foodList
             let el =foodList[index]
              console.log(1);
+            //  ========================================
             this.foodsScroll.scrollToElement(el,300);
+            // ========================================
            
       },
       _initScroll(){
@@ -112,6 +129,7 @@ import shopcart from '../shopcart/shopcart'
           })  //滚动时  获取滚动位置的正整数值
       },
       _calculateHeight() {
+        //   在dom更新完毕后运行
           let foodList = this.$refs.foodList;
           let height = 0;
           this.listHeight.push(height);
@@ -124,7 +142,8 @@ import shopcart from '../shopcart/shopcart'
       }
   },
   components: {
-      shopcart
+      shopcart,
+      cartcontrol
   }
  }
 </script>
@@ -132,38 +151,38 @@ import shopcart from '../shopcart/shopcart'
 <style scoped lang="stylus">
 @import "../../common/stylus/mixin"
   .goods
-    display: flex
-    position: absolute
-    top: 174px
-    bottom: 46px
-    width: 100%
-    overflow: hidden
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
     .menu-wrapper
-      flex: 0 0 80px
-      width: 80px
-      background: #f3f5f7
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
       .menu-item
-        display: table
-        height: 54px
-        width: 56px
-        padding: 0 12px
-        line-height: 14px
+        display table
+        height 54px
+        width 56px
+        padding 0 12px
+        line-height 14px
         &.current
-          position: relative
-          z-index: 10
-          margin-top: -1px
-          background-color :#fff
-          font-weight: 700
+          position relative
+          z-index 10
+          margin-top -1px
+          background-color #fff
+          font-weight 700
           .text
             border-none()
         .icon
-          display: inline-block
-          vertical-align: top
-          width: 12px
-          height: 12px
-          margin-right: 2px
-          background-size: 12px 12px
-          background-repeat: no-repeat
+          display inline-block
+          vertical-align top
+          width 12px
+          height 12px
+          margin-right 2px
+          background-size 12px 12px
+          background-repeat no-repeat
           &.decrease
             bg-image('decrease_3')
           &.discount
@@ -175,60 +194,63 @@ import shopcart from '../shopcart/shopcart'
           &.special
             bg-image('special_3')
         .text
-          display: table-cell
-          width: 56px
-          vertical-align: middle
+          display table-cell
+          width 56px
+          vertical-align middle
           border-1px(rgba(7, 17, 27, 0.1))
-          font-size: 12px
+          font-size 12px
     .foods-wrapper
-      flex: 1
+      flex 1
       .title
-        padding-left: 14px
-        height: 26px
-        line-height: 26px
-        border-left: 2px solid #d9dde1
-        font-size: 12px
-        color: rgb(147, 153, 159)
-        background: #f3f5f7
+        padding-left 14px
+        height 26px
+        line-height 26px
+        border-left 2px solid #d9dde1
+        font-size 12px
+        color rgb(147, 153, 159)
+        background #f3f5f7
       .food-item
-        display: flex
-        margin: 18px
-        padding-bottom: 18px
+        display flex
+        margin 18px
+        padding-bottom 18px
         border-1px(rgba(7, 17, 27, 0.1))
         &:last-child
           border-none()
-          margin-bottom: 0
+          margin-bottom 0
         .icon
-          flex: 0 0 57px
-          margin-right: 10px
+          flex 0 0 57px
+          margin-right 10px
         .content
-          flex: 1
+          flex 1
           .name
-            margin: 2px 0 8px 0
-            height: 14px
-            line-height: 14px
-            font-size: 14px
-            color: rgb(7, 17, 27)
+            margin 2px 0 8px 0
+            height 14px
+            line-height 14px
+            font-size 14px
+            color rgb(7, 17, 27)
           .desc, .extra
-            line-height: 10px
-            font-size: 10px
-            color: rgb(147, 153, 159)
+            line-height 10px
+            font-size 10px
+            color rgb(147, 153, 159)
           .desc
-            line-height: 12px
-            margin-bottom: 8px
+            line-height 12px
+            margin-bottom 8px
           .extra
             .count
-              margin-right: 12px
+              margin-right 12px
           .price
-            font-weight: 700
-            line-height: 24px
+            font-weight 700
+            line-height 24px
             .now
-              margin-right: 8px
-              font-size: 14px
-              color: rgb(240, 20, 20)
+              margin-right 8px
+              font-size 14px
+              color rgb(240, 20, 20)
             .old
-              text-decoration: line-through
-              font-size: 10px
-              color: rgb(147, 153, 159)
-
+              text-decoration line-through
+              font-size 10px
+              color rgb(147, 153, 159)
+          .cartcontrol-wrapper
+              position absolute
+              right 0
+              bottom 12px
 </style>
--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -3,18 +3,24 @@
      <div class="content">
          <div class="content-left">
              <div class="logo-wrapper">
-                 <div class="logo">
-                     <i class="icon-shopping_cart"></i>
+                 <div class="logo" :class="{'highlight':totalCount>0}">
+                     <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
                  </div>
-                 <div class="number"></div>
+                 <div class="num" v-show="totalCount>0">{{ totalCount }}</div>
              </div>
-             <div class="price">￥{{ totalPrice }}</div>
+             <div class="price" :class="{'heighlight':totalPrice>0}">￥{{ totalPrice }}</div>
              <div class="desc">另需配送费￥{{deliveryprice}}元</div>
          </div>
          <div class="content-right">
-             <div class="pay">
-                 ￥{{ minprice }}元配送
+             <div class="pay" :class="payClass">
+                 {{ payDesc }}
              </div>
+         </div>
+         <div class="ball-container">
+             <transition name="drop">
+                <div v-for="(ball,index) in balls" v-show="ball.show" :key="index" class="ball"></div>
+                <div class="innerr"></div>        
+             </transition>       
          </div>
      </div>
  </div>
@@ -29,7 +35,7 @@
             return [
                 {
                     price:10,
-                    count:1
+                    count:2
                 }
             ];
         }
@@ -45,24 +51,56 @@
    }  ,
    data() {
      return {
-
+         balls:[
+             {
+                 show:false
+             },
+             {
+                 show:false
+             },
+             {
+                 show:false
+             },
+             {
+                 show:false
+             },
+             {
+                 show:false
+             },
+         ]
      }
-
    },
    computed: {
     totalPrice() {
         let total = 0;
-        this.selectFoods.forEach((food) => {
-            total += food.price * food.count;
+        this.selectFoods.forEach((item) => {
+            total += item.price * item.count;
             })
             return total;
     },
     totalCount() {
         let count = 0;
-        this.selectFoods.forEach((food)=>{
-            count += food.count;
+        this.selectFoods.forEach((item)=>{
+            count += item.count;
         });
         return count
+    },
+    payDesc() {
+        if(this.totalPrioce === 0){
+            return `￥$(this.mimprice)元起送`;
+        }else if (this.totalPrice<this.minprice){
+            let diff = this.minprice - this.totalPrice
+            return `还差${diff}元起送`
+        }else{
+            return '去结算'
+        }
+    },
+    payClass() {
+        if(this.totalPrice<this.minprice){
+            return 'not-enough';
+        }else {
+            return 'enough'
+        }
     }
    }
  }
@@ -101,10 +139,29 @@
                border-radius 50%
                text-align center
                background-color #243b3c  
+               &.highlight
+                  background-color rgb(0,160,220)
                .icon-shopping_cart
                    font-size 24px
                    color #80858a
                    line-height 44px
+                   &.highlight
+                      color #fff
+           .num  
+             position absolute
+             top 0
+             right 0
+             width 24px
+             height 16px      
+             line-height 16px
+             text-align center
+             border-radius 16px
+             font-weight 700
+             color #ffffff
+             font-size 9px
+             background-color rgb(240,20,20)
+             box-shadow 0 4px 8px 0 rgba(0,0,0,0.4)
+
            .price
              display inline-block
              vertical-align top
@@ -117,6 +174,8 @@
              font-size 16px
              font-weight 700
              color rgba(255,255,255,0.4)
+             &.heighlight
+               color #ffffff
            .desc    
              display inline-block
              vertical-align top
@@ -124,6 +183,7 @@
              margin 12px 0 0 12px
              color rgba(255,255,255,0.4)
              font-size 10px
+       
         .content-right
             flex 0,0,105px
             width 105px      
@@ -135,4 +195,25 @@
               color rgba(255,255,255,0.4)
               font-weight 700
               background-color #2b333b
+              &.not-enough
+                 background-color #2b333b
+              &.enough
+                 background-color #00b43c
+                 color #fff
+      .ball-container
+         .ball
+           position fixed
+           left 32px
+           bottom 22px
+           z-index 200
+           &.drop-enter-active,&.drop-leave-active
+               transition all 0.4s
+               .inner  
+                 width 16px
+                 height 16px
+                 border-radius 50%
+                 background-color rgb(0,160,220)  
+
+                 
+                 
 </style>
